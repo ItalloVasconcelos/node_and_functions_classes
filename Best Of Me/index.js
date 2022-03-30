8236968149dc559054d7261d6d2546cf39e07a0c
Itallo Vasconcelos <itallo.savio.vasconcelos61@ifce.aluno.edu.br>
2022-03-30 15:51:59 -0300

    feat: over the modifications in my app

--- Best Of Me/index.js
+++ Best Of Me/index.js
@@ -1,3 +1,4 @@
+// Criando a variável com uma array para as perguntas
 const questions = [
     "O que aprendi hoje?",
     "O que me chateou?",
@@ -5,15 +6,17 @@ const questions = [
     "O que me deixou contente hoje?",
     "Quantas pessoas ajudei hoje?"
 ]
-
+// Criando a função para começar na pergunta 1 no index 0 da array e adicionar um '>'
 const ask = ( index = 0 )  => {
   process.stdout.write("\n\n" + questions [index] + " > ")
 }
-
+// Chamando a função
 ask()
 
+// Criando uma array vazia para adicionar as respostas
 const answers = []
 
+//Criando a função onde armazena os dados e transforma em strings. Depois passa por um if para saber se as respostas são maiores que as perguntas, caso seja positivo o programa continua rodando, se não o programa da um exit
 process.stdin.on("data", data => {
     process.stdout.write( data.toString().trim() + "\n")
     answers.push(data.toString().trim() + "\n")
@@ -23,4 +26,22 @@ process.stdin.on("data", data => {
         console.log(answers)
         process.exit()
     }
+})
+
+//Criando uma visualização mais organizada das respostas.
+process.on('exit', () => {
+    console.log(`Bacana Itallo!
+    O que você aprendeu hoje?
+    ${answers[0]}
+
+    O que te aborreceu e como você poderia melhorar foi:
+    ${answers[1]}
+    
+    O que te deixou feliz hoje:
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!!
+
+    Volte amanhã para novas reflexões
+    `)
 })
